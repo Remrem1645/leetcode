1fc128087978697a260497b9acbc6616abf348ea
Remrem1645 <dyan@cpp.edu>
2022-08-14 03:14:45 -0700

    num 4 done

--- JS/0004. Median of Two Sorted Arrays.js
+++ JS/0004. Median of Two Sorted Arrays.js
@@ -28,7 +28,6 @@ var findMedianSortedArrays = function(nums1, nums2) {
 
 nums1 = [], nums2 = [1,2,3,4,5]
 
-
 console.log(findMedianSortedArrays(nums1,nums2)); 
 
 
@@ -40,20 +39,19 @@ The overall run time complexity should be O(log (m+n)).
 
 
 Example 1:
-
 Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
-Example 2:
 
+Example 2:
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 
-[2,5,8 , 9]
+[2,5,8,9]
 
-[4, 5, 6, 7]
+[1,4,6,7]
 
 
 */

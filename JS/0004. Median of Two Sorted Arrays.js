abfcceeb3b8cad9f6b6d6dd88ce52c5008b98199
Remrem1645 <dyan@cpp.edu>
2022-08-14 03:13:28 -0700

    num 4 comepleted

--- JS/0004. Median of Two Sorted Arrays.js
+++ JS/0004. Median of Two Sorted Arrays.js
@@ -5,46 +5,31 @@
 https://leetcode.com/problems/median-of-two-sorted-arrays/
 
 */
-/* 
+
 var findMedianSortedArrays = function(nums1, nums2) {
-    let mid1 = 0;
-    let mid2 = 0;
-
-    if(nums1.length !== 0){
-        let start1 = 0;
-        let end1 = nums1.length - 1;
-        if(nums1.length % 2){
-            let middle = Math.floor((start1 + end1) / 2)
-            mid1 = nums1[middle];
-        }else{
-            let middle = Math.floor((start1 + end1) / 2);
-            mid1 = ((nums1[middle] + nums1[middle + 1]) / 2);
+    let len = (Math.floor((nums1.length + nums2.length)/2)) + 1;
+    let arr = []; let a = 0; let b = 0;
+    while(arr.length !== len){
+        if(nums1[a] <= nums2[b] && nums1[a] !== undefined|| nums2[b] === undefined){
+            arr.push(nums1[a]);
+            a++;
+        } else{
+            arr.push(nums2[b]);
+            b++;
         }
     }
 
-    if(nums2.length !== 0){
-        let start2 = 0;
-        let end2 = nums2.length - 1;
-        
-        if(nums2.length % 2){
-            let middle = Math.floor((start2 + end2) / 2)
-            mid2 = nums2[middle];
-        }else{
-            let middle = Math.floor((start2 + end2) / 2);
-            mid2 = ((nums2[middle] + nums2[middle + 1]) / 2);
-        }
+    if((nums1.length + nums2.length) % 2 == 1){
+        return arr[len - 1];
+    }else{
+        return (arr[len - 1] + arr[len - 2])/2;
     }
-
-    if(nums1.length == 0 && nums2.length == 0) return [];
-    if(nums1.length == 0) mid1 = mid2;
-    if(nums2.length == 0) mid2 = mid1;
-
-    return ((mid1 + mid2) / 2)
 };
 
-nums1 = [], nums2 = [1]
+nums1 = [], nums2 = [1,2,3,4,5]
 
-console.log(findMedianSortedArrays(nums1,nums2)); */
+
+console.log(findMedianSortedArrays(nums1,nums2)); 
 
 
 /*
@@ -66,20 +51,9 @@ Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 
-comparer for num1 and num2
+[2,5,8 , 9]
 
-[5,6,7,8,9]
-
-7
-
-[10,11,12,13,14]
-
-12
-
-12 + 7 = 19
-
-9.5
-
+[4, 5, 6, 7]
 
 
 */

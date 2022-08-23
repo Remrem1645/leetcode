5b678fdd086f3a2638154e4fd59fc80db3f3c7b0
Remrem1645 <dyan@cpp.edu>
2022-08-23 02:53:40 -0700

    Completed

--- JS/0101. Symmetric Tree.js
+++ JS/0101. Symmetric Tree.js
@@ -6,28 +6,25 @@ https://leetcode.com/problems/symmetric-tree/
 
 */
 
+
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val)
     this.left = (left === undefined ? null : left)
     this.right = (right === undefined ? null : right)
 }
 
-// creates Binary Tree from array
-
 var isSymmetric = function(root) {
     if(root == null) return null;
-
     function sysmmetric(rootLeft, rootRight){
         if(rootLeft == null && rootRight == null) return true;
         if(rootLeft == null || rootRight == null) return false;
         if(rootLeft.val !== rootRight.val) return false;
         return sysmmetric(rootLeft.left, rootRight.right) && sysmmetric(rootLeft.right, rootRight.left);
     }
-
     return sysmmetric(root.left, root.right);
 };
 
-console.log(isSymmetric(tree));
+console.log(isSymmetric(root));
 
 
 /*
--- JS/0268. Missing Number.js
+++ JS/0268. Missing Number.js
@@ -6,6 +6,7 @@ https://leetcode.com/problems/missing-number/
 
 */
 
+
 var missingNumber = function(nums) {
     let len = nums.length, total = (len * (len + 1)) / 2;
     for(i = 0; i < len; i++) total -= nums[i];
@@ -19,17 +20,16 @@ console.log(missingNumber(nums))
 /*
 
 Example 1:
-
 Input: nums = [3,0,1]
 Output: 2
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
-Example 2:
 
+Example 2:
 Input: nums = [0,1]
 Output: 2
 Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
-Example 3:
 
+Example 3:
 Input: nums = [9,6,4,2,3,5,7,0,1]
 Output: 8
 Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.

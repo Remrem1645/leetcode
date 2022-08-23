2275926c366a05770c4cd92f19d2bcedc90d31d5
Remrem1645 <dyan@cpp.edu>
2022-08-23 02:32:39 -0700

    Completed

---
+++ JS/0101. Symmetric Tree.js
@@ -0,0 +1,38 @@
+/*
+
+101. Symmetric Tree
+
+https://leetcode.com/problems/symmetric-tree/
+
+*/
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+// creates Binary Tree from array
+
+var isSymmetric = function(root) {
+    if(root == null) return null;
+
+    function sysmmetric(rootLeft, rootRight){
+        if(rootLeft == null && rootRight == null) return true;
+        if(rootLeft == null || rootRight == null) return false;
+        if(rootLeft.val !== rootRight.val) return false;
+        return sysmmetric(rootLeft.left, rootRight.right) && sysmmetric(rootLeft.right, rootRight.left);
+    }
+
+    return sysmmetric(root.left, root.right);
+};
+
+console.log(isSymmetric(tree));
+
+
+/*
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+*/

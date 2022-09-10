79753b8834dfd92295d64e1f43e36b6782c913db
Remrem1645 <dyan@cpp.edu>
2022-09-10 02:02:53 -0700

    Completed

--- JS/0141. Linked List Cycle.js
+++ JS/0141. Linked List Cycle.js
@@ -1,5 +1,21 @@
+/*
+
+141. Linked List Cycle
+
+https://leetcode.com/problems/linked-list-cycle/
+
+*/
+
 var hasCycle = function(head) {
+    let map = new Set();
     
+    function check(head){
+        if(map.has(head)) return true;
+        if(!head) return false;
+        map.add(head);
+        return check(head.next);
+    }
+    return check(head);
 };
 
 
@@ -7,3 +23,4 @@ function ListNode(val) {
     this.val = val;
     this.next = null;
 }
+

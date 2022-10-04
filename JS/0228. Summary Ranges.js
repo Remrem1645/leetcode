6eb2cdc7d821193c7db8d9484b9873ef2ab12256
Remrem1645 <dyan@cpp.edu>
2022-10-04 01:53:47 -0700

    Completed

---
+++ JS/0228. Summary Ranges.js
@@ -0,0 +1,61 @@
+/*
+
+228. Summary Ranges
+
+https://leetcode.com/problems/summary-ranges/
+
+*/
+
+
+var summaryRanges = function(nums) {
+    out = [];
+    for(let i = 1; i <= nums.length; i++){
+        if(nums[i] - nums[i - 1] == 1){
+            temp = i - 1;
+            while(nums[i] - nums[i - 1] == 1) i++;
+            out.push(`${nums[temp]}->${nums[i - 1]}`)
+        }
+        else out.push(`${nums[i - 1]}`);
+    }
+    return out;
+};
+
+Input: nums = [0,1,2,4,5,7]
+console.log(summaryRanges(nums));
+
+
+/*
+
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+*/

e043dd3ce7c359912209fab8c4365733ce323499
Remrem <dyan@cpp.edu>
2022-10-14 02:50:27 -0700

    Completed

--- JS/0012. Integer to Roman.js
+++ JS/0012. Integer to Roman.js
@@ -2,15 +2,33 @@
 
 
 var intToRoman = function(num) {
-    dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L': 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    let dict = { 1 : 'I', 4 : 'IV', 5 : 'V', 9 : 'IX', 10 : 'X', 40 : 'XL',  50 : 'L', 90 : 'XC', 100 : 'C', 400: 'CD', 500 : 'D', 900 : 'CM', 1000 : 'M'} , out = '';
 
-    return dict.get(nums);
+    for(let i = 0, currBiggest = 1000; i < 3; i++, currBiggest /= 10){
+        let temp = currBiggest;
+        if(num >= temp) toRoman(temp);
+        temp = temp - temp / 10;
+        if(num >= temp) toRoman(temp);
+        temp = currBiggest / 2;
+        if(num >= temp) toRoman(temp);
+        temp = temp - temp / 5;
+        if(num >= temp) toRoman(temp);
+    }
+
+    if(num >= 1) toRoman(1);
 
 
+    function toRoman(currBiggest){
+        let temp = Math.floor(num / currBiggest);
+        out += `${ dict[currBiggest].repeat(temp)}`;
+        num -= temp * currBiggest;
+    }
+
+    return out;
 }
 
 
-num = 5;
+num = 1994;
 console.log(intToRoman(num))
 
 

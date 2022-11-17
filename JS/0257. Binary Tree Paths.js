624cbd197da9c7ee8d03a43f6b64e68d7d5a51d5
Remrem <dyan@cpp.edu>
2022-11-17 01:44:13 -0800

    Completed

--- JS/0257. Binary Tree Paths.js
+++ JS/0257. Binary Tree Paths.js
@@ -35,3 +35,25 @@ var binaryTreePaths = function(root) {
 
     return out;
 };
+
+
+/*
+
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+
+
+*/
